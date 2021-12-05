8f1489adacf94f72b3bce1a53a35743090834459
fikripy <95403190+fikripy@users.noreply.github.com>
2021-12-04 18:57:33 -0800

    pertama komit
    coba

---
+++ sgf.py
@@ -0,0 +1,11 @@
+#pip install numpy pandas (py -m pip install matplotlib plotly_express)
+#pip install -U scikit-learn (streamlit streamlit-lottie streamlit-embedcode)
+#pip install -U featuretools (lazypredict pycaret seaborn statsmodels)
+
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import seaborn as sns, streamlit as st, statsmodels
+from scipy import stats
+import plotly.express as px
+
+print('kita mulai')
+pass
